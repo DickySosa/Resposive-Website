d8a4606b3e9484857ee849f0f512fe851394d9dc
DickySosa <127916303+DickySosa@users.noreply.github.com>
2023-04-11 20:05:43 -0600

    2 files modified

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 // // Import stylesheets
 import './style.css';
 
-let git = document.querySelector('.github')
+//links
 
-git.addEventListener('click', function(){
-  window.location.href = "https://github.com/DickySosa"
-})
+let githubLink = document.querySelector('.github');
+
+let linkedinLink = document.querySelector('.linkedin');
+
+let aditionalContacts = document.querySelector('.aditionalContacts');
+
+let resumen = document.querySelector('.resumen');
+
+let aboutMe = document.querySelector('.about');
+
+let proyects = document.querySelector('.proyects');
+
+let me = document.querySelector('.io');
+
+// event listeners
+githubLink.addEventListener('click', function () {
+  window.location.href = 'https://github.com/DickySosa';
+});
+
+linkedinLink.addEventListener('click', function () {
+  window.location.href = ''; /*add link here*/
+});
+
+aditionalContacts.addEventListener('click', function () {
+  window.location.href = ''; /*add link here*/
+});
+
+resumen.addEventListener('click', function () {
+  window.location.href = ''; /*add link here*/
+});
+
+aboutMe.addEventListener('click', function () {
+  window.location.href = ''; /*add link here*/
+});
+
+proyects.addEventListener('click', function () {
+  window.location.href = ''; /*add link here*/
+});
+
+me.addEventListener('click', function () {
+  window.location.href = ''; /*add link here*/
+});
 
 // // Write Javascript code!
 // const appDiv = document.getElementById('app');
