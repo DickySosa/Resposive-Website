15b44ca1c21c0b14260d7826312782c3f9500eb5
DickySosa <127916303+DickySosa@users.noreply.github.com>
2023-04-11 19:39:49 -0600

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // // Import stylesheets
 import './style.css';
 
+let git = document.querySelector('.github')
+
+git.addEventListener('click', function(){
+  window.location.href = "https://github.com/DickySosa"
+})
+
 // // Write Javascript code!
 // const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
